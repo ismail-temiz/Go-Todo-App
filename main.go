7ab2e58db9c806b356c06b9f9c1c1f85095e9ee8
ismailtemizsky <ismail.temiz@sky.uk>
2023-02-13 17:13:35 +0000

    add files

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"html/template"
+)
+
+var tmpl *template.Template
+
+type Todo struct{
+	Item string
+	Done bool
+}
+
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+func todo(w http.ResponseWriter, r *http.Request) {
+
+	data := PageData {
+		Title: "TODO List",
+		Todos: []Todo {
+			{Item: "Install GO", Done: true},
+			{Item: "Learn Go", Done: false},
+			{Item: "Set up GoLang App", Done: false},
+			{Item: "Set up GitHub Authentication", Done: false},
+		},
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func main()  {
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+	mux.HandleFunc("/todo", todo)
+
+	log.Fatal(http.ListenAndServe(":9091", mux))
+}
